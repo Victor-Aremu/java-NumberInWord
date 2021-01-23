6b583d3efba2b3d1035a7dee2f28c91c84450437
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-23 09:41:03 +0000

    changes made

---
+++ src/NumberInWord.java
@@ -0,0 +1,29 @@
+public class NumberInWord {
+    public static void printNumberInWord(int num){
+        String number;
+        if (0 == num) {
+            number = "ZERO";
+        } else if (1 == num) {
+            number = "ONE";
+        } else if (2 == num) {
+            number = "TWO";
+        } else if (3 == num) {
+            number = "THREE";
+        } else if (4 == num) {
+            number = "FOUR";
+        } else if (5 == num) {
+            number = "FIVE";
+        } else if (6 == num) {
+            number = "SIX";
+        } else if (7 == num) {
+            number = "SEVEN";
+        } else if (8 == num) {
+            number = "EIGHT";
+        } else if (9 == num) {
+            number = "NINE";
+        } else {
+            number = "OTHER";
+        }
+        System.out.println(number);
+    }
+}
